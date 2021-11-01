ce3289ec8a98d44f98f7eab83b579766ab74b88a
Yifan - Macbook Pro <zyfinori@outlook.com>
2021-11-01 01:12:22 -0400

    allowed cors

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20211031175055) do
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
