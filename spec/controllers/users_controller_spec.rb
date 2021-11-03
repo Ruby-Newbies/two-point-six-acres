3a07e259d2bdba3a207aeec025c7a9435d2b7d6f
Tianyou Song <songtianyou07@gmail.com>
2021-11-03 14:56:28 -0400

    Add Rspec for get and list & update deprecated lines

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,17 +15,10 @@ if RUBY_VERSION>='2.6.0'
   end
 end
 
-RSpec.describe Api::V1::CommentsController, type: :controller do
-  describe "list comments" do
-    it "returns the list of comments" do
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe "list users" do
+    it "returns the list of users" do
       response = get :index
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe "get comment by id" do
-    it "returns an comment with certain id" do
-      response = get :show, id: 1
       expect(response).to have_http_status(200)
     end
   end
