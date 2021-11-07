2a3dc6d2b1f0dd7b7fec2889c3a35e3eecb20d61
Wenye Ma <mwysjtu@gmail.com>
2021-11-06 23:53:54 -0400

    list comments by article id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ActiveRecord::Base
+  def self.with_article_id(comments_of_the_article_to_show)
+    if comments_of_the_article_to_show.nil?
+      return Comment.all
+    else
+      return Comment.where(article_id: comments_of_the_article_to_show)
+    end
+  end
 end
