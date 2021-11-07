8f4b8337d910b5782f9ad77b48ff0e3c401a714d
Wenye Ma <mwysjtu@gmail.com>
2021-11-06 23:31:40 -0400

    section and user_id filters of articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
   ARTICLES_PER_PAGE = 2
   def index
-    # http://localhost:3000/api/v1/articles?page=1
+    # http://localhost:3000/api/v1/articles?page=0&author_id=1&section_id=1
     @page = params.fetch(:page,0).to_i
-    @articles = Article.offset(@page * ARTICLES_PER_PAGE).limit(ARTICLES_PER_PAGE)
+    @section_to_show = params[:section_id]
+    @author_id_to_show = params[:author_id]
+    @articles = Article.with_section(@section_to_show).with_author(@author_id_to_show).offset(@page * ARTICLES_PER_PAGE).limit(ARTICLES_PER_PAGE)
   end
 
   def show
