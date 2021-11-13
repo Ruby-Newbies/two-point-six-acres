404c2249cb413b07f6c30b2a76527ef1abccdbba
Wenye Ma <mwysjtu@gmail.com>
2021-11-13 18:33:15 -0500

    authorization

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -16,21 +16,62 @@ class Api::V1::ArticlesController < Api::V1::BaseController
 
   def create
     @article = Article.new(article_params)
+    @current_user = Article.authorize(request)
+    if @current_user.nil?
+      render json: { error: 'unauthorized create' }, status: :unauthorized_create
+      return
+    end
+    if @article.author_id == @current_user.id
+      @article.save
+    else
+      render json: { error: 'unauthorized create' }, status: :unauthorized_create
+    end
     @article.save
   end
 
   def update
       @article = Article.find(params[:id])
-      @article.update(article_params)
+      @current_user = Article.authorize(request)
+      if @current_user.nil?
+        render json: { error: 'unauthorized update' }, status: :unauthorized_update
+        return
+      end
+      if @article.author_id == @current_user.id
+        if params[:author_id].nil?
+          @article.update(article_params)
+          return
+        end
+        if params[:author_id]!=@current_user.id
+          render json: { error: 'cannot update' }, status: :unauthorized_update
+        end
+        @article.update(article_params)
+      else
+        render json: { error: 'unauthorized update' }, status: :unauthorized_update
+      end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    begin
+      @article = Article.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: { errors: exception.message }, status: :unauthorized
+      return
+    end
+    @current_user = Article.authorize(request)
+
+    if @current_user.nil?
+      render json: { error: 'unauthorized destroy' }, status: :unauthorized_destroy
+      return
+    end
+    if @current_user.role=="admin" || @current_user.id == @article.author_id
+      @article.destroy
+    else
+      render json: { error: 'unauthorized to delete' }, status: :unauthorized_delete
+    end
   end
 
   def article_params
-    params.require(:article).permit(:title, :content, :author_id, :section_id)
+    params.require(:article).permit(:title, :content, :author_id, :section_id,)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :section
+  belongs_to :user
   has_many :comments, dependent: :destroy
 
   def self.with_section(section_to_show)
@@ -15,6 +16,22 @@ class Article < ActiveRecord::Base
       return Article.all
     else
       return Article.where(author_id:author_id_to_show)
+    end
+  end
+
+  def self.authorize(request)
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = Token.decode(header)
+      @current_user = User.find_by_email(@decoded[:email])
+      return @current_user
+    rescue ActiveRecord::RecordNotFound => exception
+      # render json: { errors: exception.message }, status: :unauthorized
+      return
+    rescue JWT::DecodeError => exception
+      # render json: { errors: exception.message }, status: :unauthorized
+      return
     end
   end
 end
