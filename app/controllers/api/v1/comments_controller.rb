4669a8e160a810a9007b57a62d19e7f4fac3deae
lizima <46373274+lizima@users.noreply.github.com>
2021-11-15 22:05:55 -0500

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,9 +1,10 @@
-class Api::V1::CommentsController < Api::V1::BaseController
+class Api::V1::CommentsController < Api::V1::ApplicationController
   # before_action :require_login
+  before_action :authorize_request
   def index
     # http://localhost:3000/api/v1/comments?article_id=2
     @comments_of_the_article_to_show = params[:article_id]
-    @comments = Comment.with_article_id(@comments_of_the_article_to_show)
+    @comments = Comment.with_comment_id(@comments_of_the_article_to_show)
   end
 
   def show
@@ -22,29 +23,12 @@ class Api::V1::CommentsController < Api::V1::BaseController
       render json: { errors: exception.message }, status: :unauthorized
       return
     end
-    # @comment = Comment.find(params[:id])
-    if @comment.nil?
-      puts "comment not found"
-      # render json: { error: 'wrong comment id' }, status: :wrongid
-      return
-    end
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
 
-    begin
-      @decoded = Token.decode(header)
-      @current_user = User.find_by_email(@decoded[:email])
-      if @current_user.role=="admin"
-        @comment.destroy
-      else
-        render json: { error: 'unauthorized to delete' }, status: :unauthorized_delete
-      end
-    rescue ActiveRecord::RecordNotFound => exception
-      render json: { errors: exception.message }, status: :unauthorized
-    rescue JWT::DecodeError => exception
-      render json: { errors: exception.message }, status: :unauthorized
+    if @current_user.role=="admin"
+      @comment.destroy
+    else
+      render json: { error: 'unauthorized to delete' }, status: :unauthorized
     end
-
   end
 
   def comment_params
